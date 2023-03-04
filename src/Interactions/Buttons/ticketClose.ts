563854c987c3971af600a7c8cf16f79f869f8297
Ifanoxy <alwindoudou@gmail.com>
2023-03-04 11:28:04 +0100

    ticketClose.ts interaction added

---
+++ src/Interactions/Buttons/ticketClose.ts
@@ -0,0 +1,44 @@
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    ChannelType,
+    EmbedBuilder,
+    TextChannel
+} from "discord.js";
+import {EagleClient} from "../../structures/Client";
+import {DiscordColor} from "../../structures/Enumerations/Embed";
+
+export default {
+    execute(interaction: ButtonInteraction, client: EagleClient) {
+        interaction.channel.setName(`${interaction.channel.name.split("-")[0]}-lock`);
+        interaction.channel.fetch().then(channel => {
+            (channel as TextChannel).permissionOverwrites.edit(channel?.topic, {
+                ViewChannel: false
+            }).then(() => {
+                interaction.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(DiscordColor.Eagle)
+                            .setTitle("Ticket Fermé !")
+                            .setDescription(`Ticket fermé par \`${interaction.user.tag}\``)
+                            .setTimestamp()
+                    ],
+                    components: [
+                        new ActionRowBuilder<ButtonBuilder>().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId("ticketDelete")
+                                .setLabel("Supprimer le ticket")
+                                .setStyle(ButtonStyle.Danger),
+                            new ButtonBuilder()
+                                .setCustomId("ticketOpen")
+                                .setLabel("Réouvrir le ticket")
+                                .setStyle(ButtonStyle.Success),
+                        )
+                    ]
+                })
+            })
+        })
+    }
+}
