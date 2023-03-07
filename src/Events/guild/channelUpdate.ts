8662ceece9bf9d8eca8dafc183f9e70885df27e4
Ifanoxy <alwindoudou@gmail.com>
2023-03-07 09:19:57 +0100

    mass Channel Update anti raid

--- src/Events/guild/channelUpdate.ts
+++ src/Events/guild/channelUpdate.ts
@@ -1,9 +1,13 @@
 import {EagleClient} from "../../structures/Client";
-import {ChannelType, EmbedBuilder, GuildChannel} from "discord.js";
+import {AuditLogEvent, ChannelType, EmbedBuilder, GuildChannel} from "discord.js";
+import {DatabaseManager} from "../../structures/Managers/main";
+import {Antiraid} from "../../structures/Interfaces/Managers";
 
 export default {
     name: "channelUpdate",
     execute(client: EagleClient, oldChannel: GuildChannel | null, newChannel: GuildChannel) {
+        const AntiraidData = client.managers.antiraidManager.getIfExist(newChannel.guildId)
+        if (!AntiraidData?.status["anti-massChannel"]?.update?.status) this.antiraid(AntiraidData, newChannel, client);
         const channel = client.func.log.isActive(newChannel.guildId, "ChannelUpdate");
         if (!channel)return;
         let changement: {
@@ -59,5 +63,36 @@ export default {
         channel.send({
             embeds: [logEmbed]
         });
+    },
+
+    async antiraid(AntiraidData:  DatabaseManager<Antiraid> & Antiraid, channel: GuildChannel, client: EagleClient) {
+        const AuditLog = await channel.guild.fetchAuditLogs({limit: 1, type: AuditLogEvent.ChannelUpdate});
+        const userId = AuditLog.entries[0].user.id;
+        if (client.isOwner(userId))return;
+        if (AntiraidData.status["anti-massChannel"].update.ignoreWhitelist) {
+            if(client.isWhitelist(userId))return;
+        }
+        const maxfrequence = Number(AntiraidData.status["anti-massChannel"].update.frequence.split('/')[0]);
+        try {
+            var frequenceData: any = import(`../frequence/${userId}.json`);
+        } catch {
+            var frequenceData: any = {};
+        }
+        if ((frequenceData?.channelUpdate || 0) < maxfrequence-1) {
+            frequenceData.channelUpdate = (frequenceData?.channelUpdate || 0) + 1;
+            client._fs.writeFileSync(`./AntiRaid/frequence/${userId}.json`, JSON.stringify(frequenceData));
+            setTimeout(() => {
+                try {
+                    frequenceData.channelUpdate -= 1;
+                    client._fs.writeFileSync(`./AntiRaid/frequence/${userId}.json`, JSON.stringify(frequenceData));
+                } catch {}
+            }, (Number(AntiraidData.status["anti-massChannel"].update.frequence.split('/')[1].slice(0, AntiraidData.status["anti-massChannel"].update.frequence.split('/')[1].length - 1)) * 1000))
+            return;
+        }
+
+        const member = await channel.guild.members.fetch(userId);
+        await client.func.mod.applySanction(member[0], AntiraidData.status["anti-massChannel"].update.sanction, AntiraidData, "Mass Channel Create");
+        delete frequenceData?.channelUpdate;
+        client._fs.writeFileSync(`./AntiRaid/frequence/${userId}.json`, JSON.stringify(frequenceData));
     }
 }
